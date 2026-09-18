615a45ac358963b27898501f71a1923404eb5a8e
KMurray8 <kayleemurray888@gmail.com>
2026-09-18 09:32:54 -0500

    Messed up first repos, copy and paste to this one.

---
+++ incident_report.py
@@ -0,0 +1,23 @@
+import random
+time = input("Enter a time: ")
+device = input("Enter a device (include a or an): ")
+location = input("Enter a location (include the or a): ")
+suspicious_item = input("Enter a suspicious object (include a or an): ")
+severity = input("Enter a dramatic severity level: ")
+recommended_action = input("Enter a reccomended action: ")
+
+incident_summaries = [
+    f"At {time}, {device} in {location} attempted to communicate with {suspicious_item}.",
+    f"At {time}, staff in {location} discovered {suspicious_item} controlling {device}.",
+    f"Security cameras in {location} recorded {device} following {suspicious_item} at {time}.",
+    f"A report from {location} claims that {suspicious_item} tried to log in to {device} at {time}.",
+    f"A {suspicious_item} was found by {device} in {location} at {time} yesterday.",
+]
+
+summary = random.choice(incident_summaries)
+
+print()
+print("INCIDENT SUMMARY")
+print(summary)
+print(f"Severity: {severity}")
+print(f"Recommended Action: {recommended_action}")
